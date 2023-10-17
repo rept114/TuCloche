69bf912e59939da2bdd3da2d6aab0291d5971a23
AdanVaqueroLoco <adan905@hotmail.com>
2023-10-17 01:24:28 -0700

    Productos

--- index.php
+++ index.php
@@ -50,7 +50,6 @@
         <br><br> 
         <h4>Menú General:</h4><br>
         <div class="container">
-
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
                 <div class="card h-100">
