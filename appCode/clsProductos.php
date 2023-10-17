4e7df77226af8fb4656f9724cef9b20ee9cbfa4a
rept114 <busquerasegura114@gmail.com>
2023-10-17 02:40:41 -0700

    pushprueba

---
+++ appCode/clsProductos.php
@@ -0,0 +1,21 @@
+<?php
+require_once("clsMySQLPDO.php");
+
+class Productos
+{
+ 
+    public $objMysql;
+ 
+    public function __construct(){
+        //$this->conn = $db;
+        $this->objMysql = new clsMysql();
+    }
+ 
+    // regresa todos los clientes activos (status = 1)
+    public function getProductos($MQ=false){
+        //select all data       
+        return $this->objMysql->ejecutaSPSafe('sp_get_producto_DB',null,$MQ);   
+    }
+
+}
+?>
