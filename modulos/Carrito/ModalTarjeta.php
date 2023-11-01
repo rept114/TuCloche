3fb9aaec498d1d53ab8bb14f4f7807b61acf6a4c
AdanVaqueroLoco <adan905@hotmail.com>
2023-10-31 20:19:15 -0700

    modal tarjeta

--- modulos/Carrito/ModalTarjeta.php
+++ modulos/Carrito/ModalTarjeta.php
@@ -1,4 +1,4 @@
-<div class="modal-dialog">
+<div class="modal-dialog curved-table">
     <div class="modal-content">
         <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalLabel">Tarjeta de pago</h1>
