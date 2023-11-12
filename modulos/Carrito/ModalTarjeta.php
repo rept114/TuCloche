ba954e1e34fe0c5d948fa057e8d10a116cfe478c
rept114 <69810413+rept114@users.noreply.github.com>
2023-11-12 16:58:57 -0700

    Update ModalTarjeta.php

--- modulos/Carrito/ModalTarjeta.php
+++ modulos/Carrito/ModalTarjeta.php
@@ -5,28 +5,28 @@
         </div>
         <div class="modal-body">
         <form>
-        <div class="mb-3">
-            <input type="email" class="form-control" id="nombre_completo" placeholder="Nombre completo">
-        </div>
-        <div class="mb-3">
-            <input type="email" class="form-control" id="numero_tarjeta" placeholder="Número tarjeta" maxlength="19" oninput="formatTarjeta(event);">
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="mb-3">
-                    <input type="email" class="form-control" id="vencimiento" placeholder="mm/yyyy" maxlength="7" oninput="formatFecha(event)">
+            <div class="mb-3">
+                <input type="email" class="form-control" id="nombre_completo" placeholder="Nombre completo">
+            </div>
+            <div class="mb-3">
+                <input type="email" class="form-control" id="numero_tarjeta" placeholder="Número tarjeta" maxlength="19" oninput="formatTarjeta(event);">
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="vencimiento" placeholder="mm/yyyy" maxlength="7" oninput="formatFecha(event)">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="codigo_seguridad" maxlength="3" oninput="formatCodigo(event);" placeholder="CVV">
+                    </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="mb-3">
-                    <input type="email" class="form-control" id="codigo_seguridad" maxlength="3" oninput="formatCodigo(event);" placeholder="CVV">
-                </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Recordar mi tarjeta</label>
             </div>
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Recordar mi tarjeta</label>
-        </div>
         </form>
         </div>
         <div class="modal-footer d-flex justify-content-center">
